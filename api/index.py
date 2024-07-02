4b62bafedc8f427ba3cfc8979248ab1beef34852
Logan Paxton <111173907+LoganPaxton@users.noreply.github.com>
2024-07-02 13:20:26 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,15 +4,5 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, world'
-
-
-@app.route('/test')
-def test():
-    return 'Test'
-
-@app.route('/result')
-def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+def index():
+    return render_template("index.html")
